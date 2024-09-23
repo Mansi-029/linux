26a951e98955ac58bd03d92ef7d2b2b4dcc54f86
Mansi-029 <117916600+Mansi-029@users.noreply.github.com>
2024-09-23 16:08:26 +0530

    hello_module.c

--- hello_module.c
+++ hello_module.c
@@ -3,12 +3,12 @@
 #include <linux/init.h>
 
 static int __init hello_init(void) {
-    printk(KERN_INFO "Hello, Kernel!\n");
+    printk(KERN_INFO "Hello, Kernel World!\n");
     return 0;
 }
 
 static void __exit hello_exit(void) {
-    printk(KERN_INFO "Goodbye, Kernel!\n");
+    printk(KERN_INFO "Goodbye, Kernel World!\n");
 }
 
 module_init(hello_init);
